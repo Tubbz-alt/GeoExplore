571153593e0e784d1b4b07d4e56586e5eeee4aea
Marvin Smith <marvin.smith@sncorp.com>
2014-04-14 15:13:12 -0700

    adding start of main function or gui.

---
+++ src/gui/main.cpp
@@ -0,0 +1,54 @@
+/**
+ * @file    main.cpp
+ * @author  Marvin Smith
+ * @date    4/14/2014
+ */
+
+/// Qt Libraries
+#include <QApplication>
+#include <QtGui>
+#include <QWidget>
+
+/// GeoExplore library
+#include <GeoExplore.hpp>
+
+/// C++ Libraries
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+/**
+ * Main Function
+ */
+int main( int argc, char* argv[] ){
+
+    try{
+
+        /// set the filename for the config file
+        string filename;
+        filename = string(getenv("HOME")) + string("/.geoexplore/options.cfg");
+
+        // load the config file parser
+        settings.load( argc, argv, filename );
+
+    } catch ( exception& e ){
+        cout << e.what() << endl;
+    }
+
+    /// Create the application
+    QApplication app(argc, argv);
+
+    /// Create main window
+    MainWindow* mainWindow = new MainWindow();
+    
+    /// Display the main window
+    mainWindow->show();
+
+    /// Execute the application
+    return app.exec();
+
+} 
+
+
