60faeb811ec18502ce795185ca9181ab346bdfdc
Marvin Smith <marvin_smith1@me.com>
2014-05-11 10:49:42 -0700

    Adding some infrastructure to the MGRS class.

--- src/cpp/coordinate/CoordinateMGRS.hpp
+++ src/cpp/coordinate/CoordinateMGRS.hpp
@@ -7,8 +7,41 @@
 #define __SRC_CPP_COORDINATE_COORDINATEMGRS_HPP__
 
 /// GeoExplore Libraries
+#include <GeoExplore/core/Enumerations.hpp>
 #include <GeoExplore/coordinate/CoordinateBase.hpp>
 
+namespace GEO{
+
+/**
+ * @class CoordinateMGRS
+*/
+template <typename DATATYPE>
+class CoordinateMGRS : public CoordinateBase<DATATYPE>{
+
+    public:
+        
+        /// Typedef the datatype
+        typedef DATATYPE datatype;
+
+        /// Create the pointer type
+        typedef boost::shared_ptr<CoordinateMGRS<DATATYPE> > ptr_t;
+
+
+        /**
+         * Default Constructor
+        */
+        CoordinateMGRS() : CoordinateBase<DATATYPE>(){}
+
+
+    private:
+
+
+
+
+}; /// End of CoordinateMGRS Class
+
+
+} /// End of GEO Namespace
 
 
 #endif
