cbbf31d42195368ef84c2adb8adad766d2cc57b5
Marvin Smith <marvin_smith1@me.com>
2014-05-15 22:13:34 -0700

    Starting to plan out the dem module.

---
+++ src/cpp/dem/DEM_Engine.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file    DEM_Engine.hpp
+ * @author  Marvin Smith
+ * @date    5/14/2014
+*/
+#ifndef __GEOEXPLORE_DEM_DEMENGINE_HPP__
+#define __GEOEXPLORE_DEM_DEMENGINE_HPP__
+
+namespace GEO{
+
+/**
+ * @class DEM_Engine
+*/
+class DEM_Engine{
+
+    public:
+
+        /**
+         * Constructor
+        */
+
+
+} /// End of DEM_Engine Class
+
+
+} /// End of GEO Namespace
+
+#endif
