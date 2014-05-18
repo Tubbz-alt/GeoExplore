80f0eab699a895eec592723d49fa776b2bc03716
jodag <gleaso22@gmail.com>
2014-05-17 17:33:04 -0700

    Now have basic OpenGL example working

--- src/cpp/apps/terrain-explore/TerrainViewer.hpp
+++ src/cpp/apps/terrain-explore/TerrainViewer.hpp
@@ -16,8 +16,55 @@ public:
      * Default constructor
      */
     TerrainViewer(QWidget* parent = nullptr);
+    
+    /**
+     * Destructor
+     */
+    ~TerrainViewer();
+
+    /* ***********************************************************
+     *               Public Overloaded function
+     ************************************************************/
+    /**
+     * Hints for creating window at the correct size
+     */
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
 
 protected:
+    /* ***********************************************************
+     *               Protected Overloaded function
+     ************************************************************/
+    /**
+     * Initialize OpenGL
+     */
+    void initializeGL();
 
+    /**
+     * Paint function occurs every frame
+     */
+    void paintGL();
+
+    /**
+     * Called when window is resized. Change Viewport, etc...
+     */
+    void resizeGL(int width, int height);
+
+private:
+
+    /// TODO These are temporary to test OpenGL. They will be abstracted away later
+    struct Vertex
+    {
+        GLfloat position[3];
+        GLfloat color[3];
+    };
+    GLuint m_program;
+    GLuint m_vboGeometry;
+    GLint m_locPosition;
+    GLint m_locColor;
+    // END TODO
+
+    /// Holds the current window size
+    QSize m_windowSize;
 };
 
